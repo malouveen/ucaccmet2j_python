249be2cc447feab1cc978312655321ee939cd72e
Malou Veen van Wolfswinkel <m.e.veenvanwolfswinkel@students.uu.nl>
2026-01-13 10:35:01 +0100

    Precipitation: 1.3 valeus not working

--- precipitation_analysis.py
+++ precipitation_analysis.py
@@ -13,14 +13,31 @@ for seattle in precipitation_data:
 print(seattle_precipitation)
 
 # 1.3 Calculating total monthly precipitation
-total_monthly_precipitation = []
+total_monthly_precipitation = {}
 monthly_precipitation = 0
 month = 0 
 
-while month < 13:
+while month < 12:
     month += 1
+    monthly_precipitation = 0
     for measurement in seattle_precipitation:
-        if measurement['date'].startswith(f'2010-0{month}'):
+        year, month, day = measurement['date'].split('-') 
+        if date_split[1] == month:
             monthly_precipitation += measurement['value']
     total_monthly_precipitation.append(monthly_precipitation)
 print(total_monthly_precipitation)
+
+
+# 1.4 Saving results as JSON file
+results = {}
+results['Seattle'] = {
+    'station': 'GHCND:USW00093814',
+    'state': 'OH',
+    'total_monthly_precipitation': total_monthly_precipitation,
+    'total_yearly_precipitation': 0,
+    'relative_monthly_precipitation': [0] *12,
+    'relative_yearly_precipitation': 0
+}
+
+with open('results.json', 'w', encoding = 'utf-8') as file:       # Writing to a JSON file
+    json.dump(results, file, indent = 4)  
